7bb2bd2553ba67d182f217c63130b0af8c45eece
AP Pranav <pranavap96@gmail.com>
2017-07-14 05:47:28 +0530

    barebones for the sidebar is setup

--- src/components/Room.jsx
+++ src/components/Room.jsx
@@ -3,15 +3,84 @@ import { connect } from 'react-redux';
 
 import List from 'List';
 import Textbar from 'Textbar';
-//import Sidebar from 'Sidebar';
+//import MaxSidebar from 'MaxSidebar';
+import MaxSidebar from 'react-sidebar';
+
+const styles = {
+  contentHeaderMenuLink: {
+    textDecoration: 'none',
+    color: 'black',
+    padding: 8,
+  },
+  content: {
+    padding: '16px',
+  },
+};
+
+const mql = window.matchMedia(`(min-width: 800px)`);
 
 export class Room extends React.Component {
+  constructor(props) {
+   super(props);
+
+   this.state = {
+     mql,
+     docked: false,
+     open: false,
+   };
+
+   this.mediaQueryChanged = this.mediaQueryChanged.bind(this);
+   this.toggleOpen = this.toggleOpen.bind(this);
+   this.onSetOpen = this.onSetOpen.bind(this);
+ }
+
+ componentWillMount() {
+   mql.addListener(this.mediaQueryChanged);
+   this.setState({ mql, docked: mql.matches });
+ }
+
+ componentWillUnmount() {
+   this.state.mql.removeListener(this.mediaQueryChanged);
+ }
+
+ onSetOpen(open) {
+   this.setState({ open });
+ }
+
+ mediaQueryChanged() {
+   this.setState({
+     mql,
+     docked: this.state.mql.matches,
+   });
+ }
+
+ toggleOpen(ev) {
+   this.setState({ open: !this.state.open });
+
+   if (ev) {
+     ev.preventDefault();
+   }
+   //!this.state.docked &&
+ }
   render() {
+    const sidebar = <h1>this is the sidebar</h1>;
+    const sidebarProps = {
+         sidebar,
+         docked: this.state.docked,
+         open: this.state.open,
+         onSetOpen: this.onSetOpen,
+       };
+
     return (
-      <div id="room">
-        <List />
-        <Textbar />
-      </div>
+      <MaxSidebar {...sidebarProps}>
+        <div id="room">
+          <div>
+            <a onClick={this.toggleOpen.bind(this)} href="#" style={styles.contentHeaderMenuLink}>=</a>
+             <span>Responsive React Sidebar</span>
+          </div>
+          <h1>here lies the main content</h1>
+        </div>
+      </MaxSidebar>
     );
   }
 }
